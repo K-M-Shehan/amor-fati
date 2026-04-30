6fa38b8b3238b6ec5c0c7930a0bad6afab1fc1ec
K-M-Shehan <miweshshehan@gmail.com>
2026-04-30 08:12:42 +0530

    spikes kill player and spikes added to level 1
    touching a spike would reload the scene and there are multiple spikes added to level 1

--- scripts/props/spike.gd
+++ scripts/props/spike.gd
@@ -1,1 +1,10 @@
 extends Area3D
+
+func _ready():
+	connect("body_entered", _on_body_entered)
+
+func _on_body_entered(body):
+	if body.name == "Player":
+		print("Player hit spikes!")
+
+		get_tree().reload_current_scene()
