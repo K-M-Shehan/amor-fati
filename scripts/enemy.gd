908fbc2d186f4151c546f54fc61802f13aa59e1f
K-M-Shehan <miweshshehan@gmail.com>
2026-05-06 17:00:45 +0530

    fixing chasing
    much better chasing (probably final version in level 1)

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -37,27 +37,31 @@ func _on_body_entered(body):
 		print("Player killed (enemy caught)")
 		body.die()
 
+func get_lookahead_target():
+	if path.size() == 0:
+		return null
+
+	var lookahead_steps = 2  # tweak this (1–3 is good)
+
+	var index = min(lookahead_steps, path.size() - 1)
+	return path[index]
+
 func _physics_process(_delta):
 
 	var target
 
 	# if close enough to player → chase directly
-	# if global_position.distance_to(player.global_position) < 2.0:
-	#	target = player.global_position
+	if global_position.distance_to(player.global_position) < 15.0:
+		target = player.global_position
+		
+	var next_node = get_lookahead_target()
 
-	# otherwise follow graph
-	var next_node = null
-
-	if path.size() > 0:
-		next_node = path[0]
-
-	# choose smarter target
 	if next_node != null:
-		var dist_to_node = global_position.distance_to(next_node)
 		var dist_to_player = global_position.distance_to(player.global_position)
+		var dist_to_node = global_position.distance_to(next_node)
 
-		# if player is closer than next node → ignore graph
-		if dist_to_player < dist_to_node:
+		# simple, stable rule
+		if dist_to_player < 4.0:
 			target = player.global_position
 		else:
 			target = next_node
@@ -71,7 +75,9 @@ func _physics_process(_delta):
 		var target_angle = atan2(dir.x, dir.z)
 		rotation.y = lerp_angle(rotation.y, target_angle, rotation_speed * _delta)
 
-	velocity = dir * speed
+	var desired_velocity = dir * speed
+	velocity = velocity.lerp(desired_velocity, 0.15)
+	
 	move_and_slide()
 
 	# move along path
