968d7c5ee5cd07a80b15d6e795463a7d801c1873
K-M-Shehan <miweshshehan@gmail.com>
2026-05-08 14:46:13 +0530

    level trigger now works for all levels
    was previously hard-coded for level 1

--- scripts/level_trigger.gd
+++ scripts/level_trigger.gd
@@ -1,8 +1,10 @@
 extends Area3D
 
+@export var next_level: String = ""
+
 func _ready():
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body):
 	if body.name == "Player":
-		get_tree().change_scene_to_file.call_deferred("res://scenes/levels/level2.tscn")
+		get_tree().change_scene_to_file.call_deferred(next_level)
